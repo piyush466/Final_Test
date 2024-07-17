76a3da2cbfd8db3f2e07208c288c61a30150debb
piyush dravyakar <piyushdravyakar69@gmail.com>
2024-07-17 18:41:22 +0530

    adding the take screenshot method

--- pages/base_file.py
+++ pages/base_file.py
@@ -4,11 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-# from Utilities.generate_log import LogGen
+from Utilities.generates_log import LogGen
 
 class Base:
 
-    # logs = LogGen.loggenerate()
+    logs = LogGen.loggenerate()
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(self.driver, 10)
@@ -17,72 +17,73 @@ class Base:
         try:
             element = self.wait.until(EC.visibility_of_element_located(by_locator))
             element.click()
-            # self.logs.info(f"Clicked on element located by {by_locator}")
+            self.logs.info(f"Clicked on element located by {by_locator}")
         except Exception as E:
-            # self.logs.error(f"Failed to click on element located by {by_locator}: {E}")
+            self.logs.error(f"Failed to click on element located by {by_locator}: {E}")
             raise
 
     def send_key(self,by_locator,data):
         try:
             element = self.wait.until(EC.visibility_of_element_located(by_locator))
             element.send_keys(data)
-            # self.logs.info(f"Sent keys '{data}' to element located by {by_locator}")
+            self.logs.info(f"Sent keys '{data}' to element located by {by_locator}")
         except Exception as E:
-            # self.logs.error(f"Failed to send keys '{data}' to element located by {by_locator}: {E}")
+            self.logs.error(f"Failed to send keys '{data}' to element located by {by_locator}: {E}")
             raise
 
     def get_text(self, by_locator):
         try:
             element = self.wait.until(EC.visibility_of_element_located(by_locator))
             text = element.text
-            # self.logs.info(f"Retrieved text '{text}' from element located by {by_locator}")
+            self.logs.info(f"Retrieved text '{text}' from element located by {by_locator}")
             return text
         except Exception as E:
-            # self.logs.error(f"Failed to get text from element located by {by_locator}: {E}")
+            self.logs.error(f"Failed to get text from element located by {by_locator}: {E}")
             raise
 
     def get_title(self):
         try:
             title = self.driver.title
-            # self.logs.info(f"Retriveing the page {title}")
+            self.logs.info(f"Retriveing the page {title}")
             return title
         except Exception as E:
-            # self.logs.error(f"Failed to get page title: {E}")
+            self.logs.error(f"Failed to get page title: {E}")
             raise
 
     def is_displayed(self, by_locator):
         try:
             elements = self.wait.until(EC.visibility_of_element_located(by_locator))
             displayed = elements.is_displayed()
-            # self.logs.info(f"Element located by {by_locator} is {'displayed' if displayed else 'not displayed'}")
+            self.logs.info(f"Element located by {by_locator} is {'displayed' if displayed else 'not displayed'}")
             return displayed
         except Exception as E:
-            # self.logs.error(f"Failed to check if element located by {by_locator} is displayed: {E}")
+            self.logs.error(f"Failed to check if element located by {by_locator} is displayed: {E}")
             raise
 
     def drop_down(self,by_locator,visible_text):
         try:
             element = self.wait.until(EC.visibility_of_element_located(by_locator))
             select = Select(element)
-            # self.logs.info(f"Selected '{visible_text}' from dropdown located by {by_locator}")
+            self.logs.info(f"Selected '{visible_text}' from dropdown located by {by_locator}")
             return select.select_by_visible_text(visible_text)
         except Exception as E:
-            # self.logs.error(f"Failed to select '{visible_text}' from dropdown located by {by_locator}: {E}")
+            self.logs.error(f"Failed to select '{visible_text}' from dropdown located by {by_locator}: {E}")
             raise
 
     def assertion(self, actual_value, expected_value):
         try:
             assert actual_value == expected_value
-            # self.logs.info(f"Assertion passed: {actual_value} == {expected_value}")
+            self.logs.info(f"Assertion passed: {actual_value} == {expected_value}")
         except Exception as E:
             Base(self.driver).take_screenshot()
-            raise E
-            # self.logs.error(f"Assertion failed: {actual_value} == {expected_value}")
+            self.logs.error(f"Assertion failed: {actual_value} == {expected_value}, {E}")
+            raise
+
 
     def take_screenshot(self):
         timestamp = time.strftime("%Y%m%d-%H%M%S")
         screenshot_name = f"screenshot_{timestamp}.png"
-        return self.driver.save_screenshot(f"../screenshot/{screenshot_name}")
-
+        self.driver.save_screenshot(f"../screenshot/{screenshot_name}")
+        self.logs.error(f"Your test is fail please check screenshot:- {screenshot_name}")
 
 
